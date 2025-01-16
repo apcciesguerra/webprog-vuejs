d893a9be59e367b1ad8ea1de0bbc394c059a2fb0
chrstn-sgrr <ciesguerra2@student.apc.edu.ph>
2025-01-16 21:41:30 +0800

    Testing bug fix

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,15 +1,17 @@
-Vue.component('slider', {
+const { createApp, defineComponent } = Vue;
+
+const Slider = defineComponent({
     data() {
         return {
             currentSlide: 0,
             images: [
-                { src: 'https://i.imgur.com/UeLZ29L.jpeg'},
-                { src: 'https://i.imgur.com/eqhnpkk.jpeg'},
-                { src: 'https://i.imgur.com/KcrXxnt.jpeg'},
-                { src: 'https://i.imgur.com/GKBmylR.jpeg'},
-                { src: 'https://i.imgur.com/sv9Ntja.jpeg'},
-                { src: 'https://i.imgur.com/a2p9Qwq.jpeg'},
-                { src: 'https://i.imgur.com/gkJRjlk.jpeg'}
+                { src: 'https://i.imgur.com/UeLZ29L.jpeg', text: 'Image 1' },
+                { src: 'https://i.imgur.com/eqhnpkk.jpeg', text: 'Image 2' },
+                { src: 'https://i.imgur.com/KcrXxnt.jpeg', text: 'Image 3' },
+                { src: 'https://i.imgur.com/GKBmylR.jpeg', text: 'Image 4' },
+                { src: 'https://i.imgur.com/sv9Ntja.jpeg', text: 'Image 5' },
+                { src: 'https://i.imgur.com/a2p9Qwq.jpeg', text: 'Image 6' },
+                { src: 'https://i.imgur.com/gkJRjlk.jpeg', text: 'Image 7' }
             ]
         };
     },
@@ -22,15 +24,15 @@ Vue.component('slider', {
         }
     },
     template: `
-    <div class="slider">
-        <button class="slider-button" @click="prevSlide">Previous</button>
-        <img :src="images[currentSlide].src" :alt="images[currentSlide].text">
-        <button class="slider-button" @click="nextSlide">Next</button>
-        <p>{{ images[currentSlide].text }}</p>
-    </div>
+        <div class="slider">
+            <button class="slider-button" @click="prevSlide">Previous</button>
+            <img :src="images[currentSlide].src" :alt="images[currentSlide].text">
+            <button class="slider-button" @click="nextSlide">Next</button>
+            <p>{{ images[currentSlide].text }}</p>
+        </div>
     `
 });
 
-new Vue({
-    el: '#app'
-});
+const app = createApp({});
+app.component('slider', Slider);
+app.mount('#app');
