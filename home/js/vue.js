e6abea0bcb78026db46a2d518f4097d9d76f14cd
chrstn-sgrr <ciesguerra2@student.apc.edu.ph>
2025-01-16 21:56:42 +0800

    Fixed picture gallery

--- home/js/vue.js
+++ home/js/vue.js
@@ -5,13 +5,13 @@ const Slider = defineComponent({
         return {
             currentSlide: 0,
             images: [
-                { src: 'https://i.imgur.com/UeLZ29L.jpeg', text: 'Image 1' },
-                { src: 'https://i.imgur.com/eqhnpkk.jpeg', text: 'Image 2' },
-                { src: 'https://i.imgur.com/KcrXxnt.jpeg', text: 'Image 3' },
-                { src: 'https://i.imgur.com/GKBmylR.jpeg', text: 'Image 4' },
-                { src: 'https://i.imgur.com/sv9Ntja.jpeg', text: 'Image 5' },
-                { src: 'https://i.imgur.com/a2p9Qwq.jpeg', text: 'Image 6' },
-                { src: 'https://i.imgur.com/gkJRjlk.jpeg', text: 'Image 7' }
+                { src: 'https://github.com/apcciesguerra/webprog-vuejs/blob/2_CLIE_experimental/images/photo1.jpg?raw=true'},
+                { src: 'https://github.com/apcciesguerra/webprog-vuejs/blob/2_CLIE_experimental/images/photo2.jpg?raw=true'},
+                { src: 'https://github.com/apcciesguerra/webprog-vuejs/blob/2_CLIE_experimental/images/photo3.jpg?raw=true'},
+                { src: 'https://github.com/apcciesguerra/webprog-vuejs/blob/2_CLIE_experimental/images/photo4.jpg?raw=true'},
+                { src: 'https://github.com/apcciesguerra/webprog-vuejs/blob/2_CLIE_experimental/images/photo5.jpg?raw=true'},
+                { src: 'https://github.com/apcciesguerra/webprog-vuejs/blob/2_CLIE_experimental/images/photo6.jpg?raw=true'},
+                { src: 'https://github.com/apcciesguerra/webprog-vuejs/blob/2_CLIE_experimental/images/photo7.jpg?raw=true'}
             ]
         };
     },
@@ -25,9 +25,9 @@ const Slider = defineComponent({
     },
     template: `
         <div class="slider">
-            <button class="slider-button" @click="prevSlide">Previous</button>
+            <button class="slider-button" @click="nextSlide">NEXT</button>
             <img :src="images[currentSlide].src" :alt="images[currentSlide].text">
-            <button class="slider-button" @click="nextSlide">Next</button>
+            <button class="slider-button" @click="prevSlide">PREVIOUS</button>
             <p>{{ images[currentSlide].text }}</p>
         </div>
     `
