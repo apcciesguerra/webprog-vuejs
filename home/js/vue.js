78441a64c39ee2237ee40f38b0d4a926067382ac
chrstn-sgrr <ciesguerra2@student.apc.edu.ph>
2025-01-16 21:11:32 +0800

    Updated suggestion box

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,10 +1,36 @@
-const app = Vue.createApp({
+Vue.component('slider', {
     data() {
-    return {
-        message: "Hello World!",
-        vueClass: "pinkBG"
-    }
-   }
-})
-  
-app.mount('#app')
+        return {
+            currentSlide: 0,
+            images: [
+                { src: 'https://i.imgur.com/UeLZ29L.jpeg'},
+                { src: 'https://i.imgur.com/eqhnpkk.jpeg'},
+                { src: 'https://i.imgur.com/KcrXxnt.jpeg'},
+                { src: 'https://i.imgur.com/GKBmylR.jpeg'},
+                { src: 'https://i.imgur.com/sv9Ntja.jpeg'},
+                { src: 'https://i.imgur.com/a2p9Qwq.jpeg'},
+                { src: 'https://i.imgur.com/gkJRjlk.jpeg'}
+            ]
+        };
+    },
+    methods: {
+        nextSlide() {
+            this.currentSlide = (this.currentSlide + 1) % this.images.length;
+        },
+        prevSlide() {
+            this.currentSlide = (this.currentSlide - 1 + this.images.length) % this.images.length;
+        }
+    },
+    template: `
+    <div class="slider">
+        <button class="slider-button" @click="prevSlide">Previous</button>
+        <img :src="images[currentSlide].src" :alt="images[currentSlide].text">
+        <button class="slider-button" @click="nextSlide">Next</button>
+        <p>{{ images[currentSlide].text }}</p>
+    </div>
+    `
+});
+
+new Vue({
+    el: '#app'
+});
