b6a58c5d3acfa9f2e9e5347353683d1cee626541
Christian Esguerra <ciesguerra2@student.apc.edu.ph>
2025-01-24 08:37:44 +0800

    Added FoodItem2

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -7,12 +7,4 @@
   <food-item/>
 </template>
  
-<style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
-</style>
+<style></style>
