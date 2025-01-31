984395211c6c4c510b0868835f8dae0000b6baa4
Christian Esguerra <ciesguerra2@student.apc.edu.ph>
2025-01-31 01:13:13 +0000

    Added old code

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -18,4 +18,11 @@
     <ul>
       <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
     </ul>
+
+    <h1>Food</h1>
+    <personal-profile/> <br>
+    <food-item/> <br>
+    <food-item2/> <br>
+    <food-item/> <br>
+    <food-item2/> <br>
   </template>
