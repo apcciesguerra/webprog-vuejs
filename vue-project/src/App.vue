a83f4b3acc209f5528119a7a7f0db5ce059d4e44
Christian Esguerra <ciesguerra2@student.apc.edu.ph>
2025-02-04 13:14:05 +0800

    Added component

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,9 +2,6 @@
   </script>
 
   <template>
-    <ul>
-      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-    </ul>
 
     <h1>Food</h1>
     <comment-form/>
@@ -15,4 +12,5 @@
     <food-item2/> <br>
     <food-item/> <br>
     <food-item2/> <br>
+    
   </template>
